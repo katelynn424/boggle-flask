5e21982538ac1bd786d8d0c2d1aa39ebfe63dd5f
Katelynn <mccy.k@aol.com>
2023-07-25 20:59:25 -0700

    delete unnecessary test in app.py

--- app.py
+++ app.py
@@ -1,45 +1,8 @@
-# from flask import Flask, request, render_template, jsonify, session
-# from boggle import Boggle
-
-# app = Flask(__name__)
-# app.config["SECRET_KEY"] = "secret"
-
-# boggle_game = Boggle()
-
-# @app.route('/')
-# def home_page():
-#     """Make board"""
-
-#     board = boggle_game.make_board()
-#     session['board'] = board
-
-#     return render_template("index.html", board=board)
-
-# @app.route("/checking-word")
-# def check_word():
-#     """See if word is in dict"""
-
-#     word = request.args["word"]
-#     board = session["board"]
-#     res = boggle_game.check_valid_word(board, word)
-
-#     return jsonify({'result': res})
-
-
-# @app.route("/post-score", methods=["POST"])
-# def post_score():
-#     score = request.json["score"]
-#     nplays = session.get("nplays", 0)
-
-#     session["nplays"] = nplays + 1
-    
-#     return jsonify(score)
-
 from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
+app.config["SECRET_KEY"] = "secret"
 
 boggle_game = Boggle()
 
